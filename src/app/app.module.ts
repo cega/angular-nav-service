67b8389abba69dd8b6911ed27b25e8dbdf6d5f31
cega <cgalindez@gmail.com>
2021-10-14 18:27:45 -0300

    3 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +22,7 @@ import { HelloComponent } from './hello.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
     MatIconModule,
--- src/app/components/menu-list-item/menu-list-item.component.ts
+++ src/app/components/menu-list-item/menu-list-item.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {NavItem} from '../../interfaces/nav-item';
+import {Router} from '@angular/router';
+import {NavService} from '../../services/nav.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-menu-list-item',
   templateUrl: './menu-list-item.component.html',
-  styleUrls: ['./menu-list-item.component.css']
+  styleUrls: ['./menu-list-item.component.scss'],
+  animations: [
+    trigger('indicatorRotate', [
+      state('collapsed', style({transform: 'rotate(270deg)'})),
+      state('expanded', style({transform: 'rotate(360deg)'})),
+      transition('expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4,0.0,0.2,1)')
+      ),
+    ])
+  ]
 })
 export class MenuListItemComponent implements OnInit {
+  expanded?: boolean;
+  @HostBinding('attr.aria-expanded') ariaExpanded = this.expanded;
+  @Input() item!: NavItem;
+  @Input() depth!: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public navService: NavService,
+              public router: Router) {
+    if (this.depth === undefined) {
+      this.depth = 0;
+    }
   }
 
+  ngOnInit() {
+    this.navService.currentUrl.subscribe((url: string) => {
+      if (this.item.route && url) {
+        // console.log(`Checking '/${this.item.route}' against '${url}'`);
+        this.expanded = url.indexOf(`/${this.item.route}`) === 0;
+        this.ariaExpanded = this.expanded;
+        // console.log(`${this.item.route} is expanded: ${this.expanded}`);
+      }
+    });
+  }
+
+  onItemSelected(item: NavItem) {
+    if (!item.children || !item.children.length) {
+      this.router.navigate([item.route]);
+      this.navService.closeNav();
+    }
+    if (item.children && item.children.length) {
+      this.expanded = !this.expanded;
+    }
+  }
 }
