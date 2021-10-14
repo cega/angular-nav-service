12f57d6af843fe4149b783c1ac2e5e098e17f9fc
cega <cgalindez@gmail.com>
2021-10-14 18:20:46 -0300

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { NavItem } from './interfaces/nav-item';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,64 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
+  version = VERSION;
   name = 'Angular ' + VERSION.major;
+      navItems: NavItem[] = [
+        {
+            displayName: 'Disney',
+            iconName: 'videocam',
+            route: 'disney',
+            children: [
+                {
+                    displayName: 'Speakers',
+                    iconName: 'group',
+                    route: 'disney/speakers',
+                    children: [
+                        {
+                            displayName: 'Michael Prentice',
+                            iconName: 'person',
+                            route: 'disney/speakers/michael-prentice',
+                            children: [
+                                {
+                                    displayName: 'Create Enterprise UIs',
+                                    iconName: 'star_rate',
+                                    route: 'disney/speakers/michael-prentice/material-design',
+                                },
+                            ],
+                        },
+                        {
+                            displayName: 'Stephen Fluin',
+                            iconName: 'person',
+                            route: 'disney/speakers/stephen-fluin',
+                            children: [
+                                {
+                                    displayName: "What's up with the Web?",
+                                    iconName: 'star_rate',
+                                    route: 'disney/speakers/stephen-fluin/what-up-web',
+                                },
+                            ],
+                        },
+                        {
+                            displayName: 'Mike Brocchi',
+                            iconName: 'person',
+                            route: 'disney/speakers/mike-brocchi',
+                            children: [
+                                {
+                                    displayName: 'My ally, the CLI',
+                                    iconName: 'star_rate',
+                                    route: 'disney/speakers/mike-brocchi/my-ally-cli',
+                                },
+                                {
+                                    displayName: 'Become an Angular Tailor',
+                                    iconName: 'star_rate',
+                                    route: 'disney/speakers/mike-brocchi/become-angular-tailor',
+                                },
+                            ],
+                        },
+                    ],
+                }
+            ],
+        },
+    ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { MenuListItemComponent } from './components/menu-list-item/menu-list-item.component';
+import { HelloComponent } from './hello.component';
+
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
